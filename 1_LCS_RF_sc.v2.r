82138b328a80a66977c4d1694c62dc8d9534e453
Emanuele Lugato <47249461+elugato@users.noreply.github.com>
2020-08-05 12:57:05 +0200

    'updates'

--- 1_LCS_RF_sc.v2.r
+++ 1_LCS_RF_sc.v2.r
@@ -17,9 +17,8 @@ NDEP<-raster("E:\\SIT\\fra\\db\\deposition_2006_to_2010\\ndep_grid")
 WCLIM<-stack("E:\\SIT\\caprese\\meteo\\worldclim2\\clim_70-00LAEA.tif")
 EOBS<-raster("E:\\SIT\\caprese\\ven\\sripts_L\\exp\\_LINUXSERVER_soil\\VCFR\\SPT_lyr\\EOBS_1kLAEA_EU.tif")
 
-#Npp<-raster("E:\\SIT\\fra\\avNPP2k2009.tif")
  RNPPin_<-raster("E:\\SIT\\caprese\\NPP\\hanpp_europe\\RNPPin.tif")
-   Npp<-raster("E:\\SIT\\caprese\\NPP\\NPP_modis_2000-10prj.tif")		###modis 2000-2009 1 km
+  Npp<-raster("E:\\SIT\\caprese\\NPP\\NPP_modis_2000-10prj.tif")		###modis 2000-2009 1 km
 
 NFERT<-raster("E:\\SIT\\fra\\db\\map\\Ntot_fert.tif")
 
@@ -27,6 +26,12 @@ ERO<-raster("E:\\SIT\\caprese\\soil\\erosion\\rusle2015\\eros_EU28_1k.tif")
 PRM<-raster("E:\\SIT\\caprese\\soil\\geology\\parmatdom2_prj.tif")
 
 PET<-raster("E:\\SIT\\caprese\\meteo\\PET\\global-et0_annual\\ET0_EU_prj.tif")
+
+WTngb<-raster('E:\\SIT\\caprese\\soil\\moisture_WT\\WTD_ngb.tif')
+# SMwi<-raster('E:\\SIT\\caprese\\soil\\moisture_WT\\SMs/Season/SM_mean_DecFeb.tif')
+# SMsp<-raster('E:\\SIT\\caprese\\soil\\moisture_WT\\SMs/Season/SM_mean_MarMay.tif')
+# SMsu<-raster('E:\\SIT\\caprese\\soil\\moisture_WT\\SMs/Season/SM_mean_JunAug.tif')
+# SMau<-raster('E:\\SIT\\caprese\\soil\\moisture_WT\\SMs/Season/SM_mean_SepNov.tif')
 
 ###extract#########
 
@@ -51,10 +56,13 @@ PRMT<-as.factor(extract(PRM, lucasSHP, method='simple', na.rm=T))
 
 ET0<-extract(PET, lucasSHP, method='bilinear', na.rm=T)
 
+WT<-extract(WTngb, lucasSHP, method='bilinear', na.rm=T)
+# SMwn<-extract(SMwi, lucasSHP, method='bilinear', na.rm=T)
+# SMsm<-extract(SMsu, lucasSHP, method='bilinear', na.rm=T)
 
 ###binding#########
 
-lucasDB<-cbind(lucasDB, MAT, RAIN, Corg, CNm, irri, Ndep_WD_tx, NPP, EROS, Nfert, PRMT, ET0, "P_E"=RAIN-ET0, EOBid) 
+lucasDB<-cbind(lucasDB, MAT, RAIN, Corg, CNm, irri, Ndep_WD_tx, NPP, EROS, Nfert, PRMT, WT) 
 
 
 CN<-lucasDB$OC/lucasDB$N
@@ -65,7 +73,7 @@ NP<-lucasDB$N/lucasDB$P
 
 
 ###############################################   LU      ########################################################
-LU<-ifelse(lucasDB$LC09 %in% c("F00"), as.character(lucasDB$LC12), as.character(lucasDB$LC09))								##bare soil to LC15 if any crop
+LU<-ifelse(lucasDB$LC09 %in% c("F00", "A22"), as.character(lucasDB$LC12), as.character(lucasDB$LC09))								##bare soil to LC15 if any crop
 LU<-ifelse(substring(LU,1,2) %in% c("B1","B2","B3","B4","B5","B7","B8"), "CR", as.character(LU))
  LU<-ifelse(LU %in% c("E10", "E20", "E30"), "GR", as.character(LU))
  LU<-ifelse(LU %in% c("C10", "C20", "C30"), "FR", as.character(LU))
@@ -76,9 +84,8 @@ lucasDB<-cbind(lucasDB, "LU"=LU)													                  ##LU is the LC1 o
 
 
 OC_ul<-120; N_ul<-OC_ul/5
-lucasDB<-subset(lucasDB, lucasDB$OC > 2 & lucasDB$OC <OC_ul)   							########limit detection OC to 120 g/gk
-# lucasDB<-subset(lucasDB, lucasDB$K <1500)   										          ########outlayer K
-  lucasDB<-subset(lucasDB, (lucasDB$clay+lucasDB$sand) %in% (1:99))   			#########removing mssing texture and OS!!!!!
+lucasDB<-subset(lucasDB, lucasDB$OC > 2 & lucasDB$OC <OC_ul)   				########limit detection OC to 120 g/gk
+lucasDB<-subset(lucasDB, (lucasDB$clay+lucasDB$sand) %in% (1:99))   			#########removing mssing texture and OS!!!!!
 # lucasDB<-subset(lucasDB, LU %in% c("SHR", "GR", "FR", "CR"))		  				#####shape predicted dataset!!!!!!!!!!
 # lucasDB<-subset(lucasDB, LU %in% c("SHR", "E10", "E20", "E30", "C10", "C20", "C30", "CR"))				   				
 
@@ -103,7 +110,7 @@ LCS$EROS[176]<-0.32
 #pairs(OC_sc_g_kg ~ s_c_prc + pH_in_H2O + OC_tf + Ndep_WD_tx + MAT + EROS + LU, data= LCS,cex=0.9, col=alpha("black", 0.5))
 
 set.seed(419)
-RF<-randomForest(OC_sc_g_kg ~ s_c_prc + pH_in_H2O + OC_tf + Ndep_WD_tx + MAT + EROS + K, data = LCS, ntree=1000, mtry=4, importance=TRUE, na.action=na.omit)
+RF<-randomForest(OC_sc_g_kg ~ s_c_prc + pH_in_H2O + OC_tf + Ndep_WD_tx + MAT + EROS + WT, data = LCS, ntree=1000, mtry=4, importance=TRUE, na.action=na.omit)
 RF
 
 #importance(RF)
@@ -118,7 +125,7 @@ par(mfrow=c(3,3))
  partialPlot(RF, LCS, Ndep_WD_tx, main=paste0("%IncMSE=", round(importance(RF)[4],1)), xlab="N deposition")
  partialPlot(RF, LCS, MAT, main=paste0("%IncMSE=", round(importance(RF)[5],1)), xlab="MAT")
  partialPlot(RF, LCS, EROS, main=paste0("%IncMSE=", round(importance(RF)[6],1)), xlab="erosion")
- partialPlot(RF, LCS, K, main=paste0("%IncMSE=", round(importance(RF)[7],1)), xlab="K")
+ partialPlot(RF, LCS, WT, main=paste0("%IncMSE=", round(importance(RF)[7],1)), xlab="WT")
 
 
 predicted<-predict(RF, LCS)
@@ -143,7 +150,7 @@ plot((LCS$OC_tf-predicted), LCS$OC_pom_g_kg)
 #RFn<-randomForest(N_sc_g_kg ~ s_c_prc + pH_in_H2O + N_tf + OC_tf + RAIN + Ndep_WD_tx + CEC, data = LCS, ntree=1000, mtry=5, importance=TRUE, na.action=na.omit)
 
 set.seed(432)
-RFn<-randomForest(N_sc_g_kg ~ s_c_prc + pH_in_H2O + N_tf + Ndep_WD_tx + MAT + EROS + K, data = LCS, ntree=1000, mtry=4, importance=TRUE, na.action=na.omit)
+RFn<-randomForest(N_sc_g_kg ~ s_c_prc + pH_in_H2O + N_tf + Ndep_WD_tx + MAT + EROS + WT, data = LCS, ntree=1000, mtry=4, importance=TRUE, na.action=na.omit)
 RFn
 
 #importance(RFn)
@@ -158,7 +165,7 @@ par(mfrow=c(3,3))
  partialPlot(RFn, LCS, Ndep_WD_tx, main=paste0("%IncMSE=", round(importance(RFn)[4],1)), xlab="N deposition")
  partialPlot(RFn, LCS, MAT, main=paste0("%IncMSE=", round(importance(RFn)[5],1)), xlab="MAT")
  partialPlot(RFn, LCS, EROS, main=paste0("%IncMSE=", round(importance(RFn)[6],1)), xlab="erosion")
- partialPlot(RFn, LCS, K, main=paste0("%IncMSE=", round(importance(RFn)[7],1)), xlab="K")
+ partialPlot(RFn, LCS, WT, main=paste0("%IncMSE=", round(importance(RFn)[7],1)), xlab="WT")
 
 
 predictedn<-predict(RFn, LCS)
@@ -193,8 +200,8 @@ sand<-stack('E:/SIT/caprese/ven/sripts_L/exp/_LINUXSERVER_soil/VCFR/SPT_lyr/soil
  silt_clay<- 100-sand                                                                            
 
 ##layer n2 
-pH_H2O<-raster('E:/SIT/caprese/soil/LUCAS/_LUCAS09_int/pH_H2O_20181023.tif') 
- pH_H2O<-resample(pH_H2O, silt_clay)   
+pH_H2O<-stack('E:/SIT/caprese/ven/sripts_L/exp/_LINUXSERVER_soil/VCFR/SPT_lyr/soil_LCS.tif')[[8]] /10
+    
 
 ##layer n3 SOC
 SOC20st_1k<-raster('E:/SIT/caprese/soil/SOC_LUCAS/export/OCstk20_c_bl.tif')/1000     ###kg/hh to Mg/ha
@@ -219,13 +226,13 @@ NDEP1k<-resample(NDEP, silt_clay)
 
 ##layer n5 
 MAT1k<-resample(WCLIM[[1]], silt_clay)  
+RAIN1k<-resample(WCLIM[[2]], silt_clay)  
 
 ##layer n6 
 EROS1k<-ERO#resample(ERO, silt_clay)  
 
 ##layer n7 
-K<-raster('E:/SIT/caprese/soil/LUCAS/_LUCAS09_int/K_20181221.tif') 
-K<-resample(K, silt_clay)   
+WTngb
 
 
 ###SOC###
@@ -233,8 +240,8 @@ K<-resample(K, silt_clay)
 
 
 inputR<-silt_clay															
-inputR<-addLayer(inputR, pH_H2O, SOC20c_1k, NDEP1k, MAT1k, EROS1k, K)			
-names(inputR)<-c('s_c_prc', 'pH_in_H2O',  'OC_tf',  'Ndep_WD_tx',  'MAT', 'EROS', 'K') 
+inputR<-addLayer(inputR, pH_H2O, SOC20c_1k, NDEP1k, MAT1k, EROS1k, WTngb)			
+names(inputR)<-c('s_c_prc', 'pH_in_H2O',  'OC_tf',  'Ndep_WD_tx',  'MAT', 'EROS', 'WT') 
 
 
 maom_1k<- predict(inputR, RF, type='response', progress='window', na.rm=T)
@@ -250,7 +257,7 @@ m_t_SOCr<-maom_1k/SOC20c_1k
 ###soil N### 
  
 inputR[[3]]<-N_1k 
-names(inputR)<-c('s_c_prc', 'pH_in_H2O',  'N_tf',  'Ndep_WD_tx',  'MAT', 'EROS', 'K') 
+names(inputR)<-c('s_c_prc', 'pH_in_H2O',  'N_tf',  'Ndep_WD_tx',  'MAT', 'EROS', 'WT') 
  
  maoN_1k<- predict(inputR, RFn, type='response', progress='window', na.rm=T)
  poN_1k<- N_1k - maoN_1k
@@ -303,13 +310,13 @@ MAON_LU<-zonal(maoN_1k*BDm_c_1k*cm_prof*0.1 , CLC, 'sum', na.rm=T)    ###N
                 'shrub-herb.', 'shrub-herb.', 'shrub-herb.', 'shrub-herb.')) 
 
 #################monetary value########################
-cur_p<-25  	##price in Euro per t of CO2
-p_mrt<-4	##ratio mrt
+#cur_p<-25  	##price in Euro per t of CO2
+#p_mrt<-4	##ratio mrt
 
-tot_e<-cur_p*3.67*(sum(fraction$MAOM)+sum(fraction$POM))/10000000         ###billion euro
+#tot_e<-cur_p*3.67*(sum(fraction$MAOM)+sum(fraction$POM))/10000000         ###billion euro
 
-pom_p<-tot_e/((sum(fraction$MAOM)*p_mrt+ sum(fraction$POM))*3.67/10000000)	 ###euro per t of CO2 
-maom_p<-pom_p*p_mrt
+#pom_p<-tot_e/((sum(fraction$MAOM)*p_mrt+ sum(fraction$POM))*3.67/10000000)	 ###euro per t of CO2 
+#maom_p<-pom_p*p_mrt
 
 ######################################################
 
@@ -322,8 +329,8 @@ fract_r<-aggregate(. ~ LC, fraction[,5:10], sum)
    fract_r$area_n<-fract_r[,2]/max(fract_r[,2])
    fract_r$mean<-fract_r$value/fract_r$area
    fract_r$CN<-fract_r$value/fract_r$value_N
-   fract_r$price<-ifelse(fract_r$variable=="POM",fract_r$value*3.67*pom_p/10000000,fract_r$value*3.67*maom_p/10000000)
-   fract_r$priceHa<-ifelse(fract_r$variable=="POM",fract_r$mean*3.67*pom_p,fract_r$mean*3.67*maom_p)
+   #fract_r$price<-ifelse(fract_r$variable=="POM",fract_r$value*3.67*pom_p/10000000,fract_r$value*3.67*maom_p/10000000)
+   #fract_r$priceHa<-ifelse(fract_r$variable=="POM",fract_r$mean*3.67*pom_p,fract_r$mean*3.67*maom_p)
      
 
 
@@ -349,17 +356,18 @@ geom_text(aes(label=round(CN,0)), position = position_stack(vjust = 0.5), size=2
 annotate("text", x = 1, y = 90, label = "C:N ratio") 
 
 
-p2<-ggplot(data=fract_r, aes(x=reorder(LC,price), y=price, fill=variable, width=area_n)) +
-   geom_bar(stat="identity") +
-  ylab("Billion Euro") + xlab("") +
-geom_text(aes(label=round(priceHa/1000,1)), position = position_stack(vjust = 0.5), size=2.8) +
-  coord_flip() +
-  theme(legend.position = "none", 
-  panel.background = element_rect(fill = "white", colour = "grey50"))+
- annotate("text", x = 1, y = 500, label = expression("keuro ha"^-1))
+#p2<-ggplot(data=fract_r, aes(x=reorder(LC,price), y=price, fill=variable, width=area_n)) +
+#   geom_bar(stat="identity") +
+#  ylab("Billion Euro") + xlab("") +
+#geom_text(aes(label=round(priceHa/1000,1)), position = position_stack(vjust = 0.5), size=2.8) +
+#  coord_flip() +
+#  theme(legend.position = "none", 
+#  panel.background = element_rect(fill = "white", colour = "grey50"))+
+# annotate("text", x = 1, y = 500, label = expression("keuro ha"^-1))
 
 
-grid.arrange(p,p1,p2, nrow =1)
+#grid.arrange(p,p1,p2, nrow =1)
+grid.arrange(p1,p, nrow =1)
  
 ################################################################################# MAOM saturation
  # f <- function(x1, x2)  ifelse(x1 %in% c(18,23,24,25,26), x2, NA)
@@ -370,41 +378,41 @@ grid.arrange(p,p1,p2, nrow =1)
  # writeRaster(MAOM_sat2-maom_1k, filename="MAOM_def.tif", format="GTiff",overwrite=T)
  
  ################################################################################# MAOM sequestration by2050
- base<-stack("E:/model/daycent/sim/lucas/results/1_km/_JEODPP/SOC_EU_DayC_grv.tif")[[6]]
-  CC<-stack("E:/model/daycent/sim/lucas/results/1_km/_JEODPP/SOC_EU_DayC_grv_CC.tif")[[6]]
-   CA<-stack("E:/model/daycent/sim/lucas/results/1_km/_JEODPP/SOC_EU_DayC_grv_ha_CA.tif")[[6]]
-   
-   
-   dCC<- CC-base ; dCC[dCC==0]<-NA ; dCC<-dCC/1.5
-   dCA<- CA-base ; dCA[dCA==0]<-NA ; dCA<-dCA/1.5
- 
-   SEQmax <- overlay(dCC, dCA, fun=max)
-   
-   SOC20c_BM<-((SOC20st_1k+SEQmax)/(BDm_c_1k*2000))*1000             ###Cseq + stock to content g/kg
-   
-   inputR<-silt_clay															
-   inputR<-addLayer(inputR, pH_H2O, SOC20c_BM, NDEP1k, MAT1k, EROS1k, K)			
-   names(inputR)<-c('s_c_prc', 'pH_in_H2O',  'OC_tf',  'Ndep_WD_tx',  'MAT', 'EROS', 'K')   
-   
-   
-   
-   maom_BM<- predict(inputR, RF, type='response', progress='window', na.rm=T)  
-   pom_BM<- SOC20c_BM - maom_BM
-   
-   dmaom_BM<- (maom_BM - maom_1k) * BDm_c_1k*cm_prof*0.1
-   dpom_BM<-  (pom_BM - pom_1k) * BDm_c_1k*cm_prof*0.1
-   
-   cellStats(dmaom_BM, sum, na.rm=T); cellStats(dpom_BM, sum, na.rm=T)
-   
-#   writeRaster(dmaom_BM*BDm_c_1k*cm_prof*0.1, filename="dmaom_BM_t_ha.tif", format="GTiff",overwrite=T)   ###stock BM
-#   writeRaster(dpom_BM*BDm_c_1k*cm_prof*0.1, filename="dpom_BM_t_ha.tif", format="GTiff",overwrite=T)   ###stock BM
-   
- 
-############################################price
-seq_price<- (dmaom_BM*maom_p + dpom_BM*pom_p)*3.67/30	
-# seq_price_<- (dmaom_BM*25 + dpom_BM*25)*3.67/30	
-
-dSQP<- seq_price - seq_price_
+#  base<-stack("E:/model/daycent/sim/lucas/results/1_km/_JEODPP/SOC_EU_DayC_grv.tif")[[6]]
+#   CC<-stack("E:/model/daycent/sim/lucas/results/1_km/_JEODPP/SOC_EU_DayC_grv_CC.tif")[[6]]
+#    CA<-stack("E:/model/daycent/sim/lucas/results/1_km/_JEODPP/SOC_EU_DayC_grv_ha_CA.tif")[[6]]
+#    
+#    
+#    dCC<- CC-base ; dCC[dCC==0]<-NA ; dCC<-dCC/1.5
+#    dCA<- CA-base ; dCA[dCA==0]<-NA ; dCA<-dCA/1.5
+#  
+#    SEQmax <- overlay(dCC, dCA, fun=max)
+#    
+#    SOC20c_BM<-((SOC20st_1k+SEQmax)/(BDm_c_1k*2000))*1000             ###Cseq + stock to content g/kg
+#    
+#    inputR<-silt_clay															
+#    inputR<-addLayer(inputR, pH_H2O, SOC20c_BM, NDEP1k, MAT1k, EROS1k, K)			
+#    names(inputR)<-c('s_c_prc', 'pH_in_H2O',  'OC_tf',  'Ndep_WD_tx',  'MAT', 'EROS', 'K')   
+#    
+#    
+#    
+#    maom_BM<- predict(inputR, RF, type='response', progress='window', na.rm=T)  
+#    pom_BM<- SOC20c_BM - maom_BM
+#    
+#    dmaom_BM<- (maom_BM - maom_1k) * BDm_c_1k*cm_prof*0.1
+#    dpom_BM<-  (pom_BM - pom_1k) * BDm_c_1k*cm_prof*0.1
+#    
+#    cellStats(dmaom_BM, sum, na.rm=T); cellStats(dpom_BM, sum, na.rm=T)
+#    
+# #   writeRaster(dmaom_BM*BDm_c_1k*cm_prof*0.1, filename="dmaom_BM_t_ha.tif", format="GTiff",overwrite=T)   ###stock BM
+# #   writeRaster(dpom_BM*BDm_c_1k*cm_prof*0.1, filename="dpom_BM_t_ha.tif", format="GTiff",overwrite=T)   ###stock BM
+#    
+#  
+# ############################################price
+# seq_price<- (dmaom_BM*maom_p + dpom_BM*pom_p)*3.67/30	
+# # seq_price_<- (dmaom_BM*25 + dpom_BM*25)*3.67/30	
+# 
+# dSQP<- seq_price - seq_price_
 
 ############################################FIGURES
 dem<-raster("E:\\SIT\\caprese\\soil\\dem\\hillshade1x1LAEA.tif") 
@@ -435,19 +443,19 @@ t2 + as.layer(t0, under = TRUE) + layer(sp.lines(mapaSHP, lwd=0.2, col='darkgray
 
 
 ####fig3
-fig3<-stack(seq_price, SEQmax/30)
-
-t3 <- levelplot(fig3, layers=1, par.settings = magmaTheme, margin = F, maxpixels=1e7, 
-scales = list(draw = FALSE), 
-colorkey=list(title = "", height=0.8, width=0.9, space="top"), main=list(label=expression("Euro ha"^-1*"yr"^-1), cex=0.8))
-t3 + as.layer(t0, under = TRUE) + layer(sp.lines(mapaSHP, lwd=0.2, col='darkgray'))
-
-
-
-t4 <- levelplot(fig3, layers=2,  par.settings = viridisTheme, margin = F, maxpixels=1e7, 
-scales = list(draw = FALSE), 
-colorkey=list(title = "", height=0.8, width=0.9, space="top"), main=list(label=expression("Mg C ha"^-1*"yr"^-1), cex=0.8))
-t4 + as.layer(t0, under = TRUE) + layer(sp.lines(mapaSHP, lwd=0.2, col='darkgray'))
+# fig3<-stack(seq_price, SEQmax/30)
+# 
+# t3 <- levelplot(fig3, layers=1, par.settings = magmaTheme, margin = F, maxpixels=1e7, 
+# scales = list(draw = FALSE), 
+# colorkey=list(title = "", height=0.8, width=0.9, space="top"), main=list(label=expression("Euro ha"^-1*"yr"^-1), cex=0.8))
+# t3 + as.layer(t0, under = TRUE) + layer(sp.lines(mapaSHP, lwd=0.2, col='darkgray'))
+# 
+# 
+# 
+# t4 <- levelplot(fig3, layers=2,  par.settings = viridisTheme, margin = F, maxpixels=1e7, 
+# scales = list(draw = FALSE), 
+# colorkey=list(title = "", height=0.8, width=0.9, space="top"), main=list(label=expression("Mg C ha"^-1*"yr"^-1), cex=0.8))
+# t4 + as.layer(t0, under = TRUE) + layer(sp.lines(mapaSHP, lwd=0.2, col='darkgray'))
 
 
 
@@ -509,17 +517,17 @@ t6 + as.layer(t0, under = TRUE) + layer(sp.lines(mapaSHP, lwd=0.2, col='darkgray
 
 
 ####supplementary seq CC
-t7 <- levelplot(dCC/30, at=seq( -0.04, 0.38, 0.02), par.settings = magmaTheme, margin = F, maxpixels=1e7, 
-scales = list(draw = FALSE), 
-colorkey=list(title = "", height=0.8, width=0.9, space="top"), main=list(label=expression("Mg C ha"^-1*"yr"^-1), cex=0.8))
-t7 + as.layer(t0, under = TRUE) + layer(sp.lines(mapaSHP, lwd=0.2, col='darkgray'))
+#t7 <- levelplot(dCC/30, at=seq( -0.04, 0.38, 0.02), par.settings = magmaTheme, margin = F, maxpixels=1e7, 
+#scales = list(draw = FALSE), 
+#colorkey=list(title = "", height=0.8, width=0.9, space="top"), main=list(label=expression("Mg C ha"^-1*"yr"^-1), cex=0.8))
+#t7 + as.layer(t0, under = TRUE) + layer(sp.lines(mapaSHP, lwd=0.2, col='darkgray'))
 
 
 ####supplementary seq price difference
-t8 <- levelplot(dSQP, par.settings = RdBuTheme, margin = F, maxpixels=1e7, 
-scales = list(draw = FALSE), 
-colorkey=list(title = "", height=0.8, width=0.9, space="top"), main=list(label=expression("Euro ha"^-1*"yr"^-1), cex=0.8))
-t8 + as.layer(t0, under = TRUE) + layer(sp.lines(mapaSHP, lwd=0.2, col='darkgray'))
+#t8 <- levelplot(dSQP, par.settings = RdBuTheme, margin = F, maxpixels=1e7, 
+#scales = list(draw = FALSE), 
+#colorkey=list(title = "", height=0.8, width=0.9, space="top"), main=list(label=expression("Euro ha"^-1*"yr"^-1), cex=0.8))
+#t8 + as.layer(t0, under = TRUE) + layer(sp.lines(mapaSHP, lwd=0.2, col='darkgray'))
 
 quantile(dSQP, probs = c(0.1, 0.5, 0.9), type=7, names = T)
 
@@ -547,9 +555,65 @@ geom_point(data = tex[1:352,], col = 'red', size=1.5)
  hist(LCS$OC_tf,  add=T, col=alpha("red", 0.2), freq=FALSE,  lty=0 )
 
 
+############################################################################### 
+############################################################################### 
+LCS<-subset(LCS, LU %in% c("SHR", "GR", "FR", "CR"))
+LCS$LU<- droplevels(LCS$LU)
+
+###POM
+
+md0<-lm(LCS$OC_pom_g_kg~LCS$MAT*LCS$LU)
+md1<-lm(LCS$OC_pom_g_kg~(LCS$MAT+LCS$RAIN)*LCS$LU)
+
+lm1<-ggplot(data=LCS, aes(MAT, OC_pom_g_kg, colour = RAIN)) + geom_point(alpha=0.5, size=1.8) + ylab("POM")+ 
+facet_wrap(~LU)+
+geom_smooth(method='lm',formula= y ~ x)+
+scale_colour_gradientn(colours = terrain.colors(10))
+
+###MAOM
+
+md0<-lm(LCS$OC_sc_g_kg~LCS$MAT*LCS$LU)
+md1<-lm(LCS$OC_sc_g_kg~(LCS$MAT+LCS$RAIN)*LCS$LU)
+
+lm1<-ggplot(data=LCS, aes(MAT, OC_sc_g_kg, colour = RAIN)) + geom_point(alpha=0.5, size=1.8) + ylab("MAOM")+ 
+facet_wrap(~LU)+
+geom_smooth(method='lm',formula= y ~ x)+
+scale_colour_gradientn(colours = terrain.colors(10))
 
 
 
+STcr<-md1$coefficients[2]
+STfr<-md1$coefficients[2]+md1$coefficients[7]
+STgr<-md1$coefficients[2]+md1$coefficients[8]
+STsh<-md1$coefficients[2]+md1$coefficients[9]
+
+SPcr<-md1$coefficients[3]
+SPfr<-md1$coefficients[3]+md1$coefficients[10]
+SPgr<-md1$coefficients[3]+md1$coefficients[11]
+SPsh<-md1$coefficients[3]+md1$coefficients[12]
 
 
+dT<- MAT1k*0+2
+dP<- RAIN1k*0-50
+
+f <- function(x1, x2, x3)  ifelse(x1 %in% c(12,13,15,16,17,19,20,21,22), x2*STcr + x3*SPcr, NA)		#CR
+ CRdt <- overlay(CLC, dT, dP, fun=f, forcefun=TRUE)
+ CRdt <- CRdt*(pom_1k*0+1)
+
+f <- function(x1, x2, x3)  ifelse(x1 %in% c(23,24,25), x2*STfr + x3*SPfr, NA)		#FR
+ FRdt <- overlay(CLC, dT, dP, fun=f, forcefun=TRUE)
+ FRdt <- FRdt*(pom_1k*0+1)
+  
+f <- function(x1, x2, x3)  ifelse(x1 %in% c(18), x2*STgr + x3*SPgr, NA)		#GR
+ GRdt <- overlay(CLC, dT, dP, fun=f, forcefun=TRUE)
+ GRdt <- GRdt*(pom_1k*0+1)
+
+f <- function(x1, x2, x3)  ifelse(x1 %in% c(26,27,28,29), x2*STsh + x3*SPsh, NA)		#SH
+ SHdt <- overlay(CLC, dT, dP, fun=f, forcefun=TRUE)
+ SHdt <- SHdt*(pom_1k*0+1)
+
+
+dMT<-merge(CRdt,FRdt,GRdt,SHdt)
+dMT<-dMT*BDm_c_1k*2				#t/ha of C
+cellStats(dMT, sum, na.mr=T)*100/1000000	
 
